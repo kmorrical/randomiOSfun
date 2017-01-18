bed12b370e537c3e2a9c955a601580c34eb6feb4
kmorrical <kathryn.morrical@gmail.com>
2017-01-18 15:38:32 -0800

    first commit

--- random/ViewController.swift
+++ random/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+    @IBOutlet weak var picLbl: UILabel!
+    
+    @IBOutlet weak var picImg: UIImageView!
+    
+    @IBAction func clickMe() {
+        let result = arc4random_uniform(6) + 1
+        
+        picLbl.text = ""
+        picImg.image = UIImage(named:
+        "\(result)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
